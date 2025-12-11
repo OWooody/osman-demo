e02659e27bee3a00a57710a143b3d5e372431f54
OWooody <osama.ynajjar@gmail.com>
2025-12-11 16:50:06 +0300

    Change page bg

--- managed-chatkit/frontend/src/App.tsx
+++ managed-chatkit/frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { ChatKitPanel } from "./components/ChatKitPanel";
 
 export default function App() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-end bg-amber-50">
+    <main className="flex min-h-screen flex-col items-center justify-end">
       <div className="mx-auto w-full max-w-[90%]">
         <ChatKitPanel />
       </div>
